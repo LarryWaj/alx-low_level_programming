f5e6802e960698828d27f713771d47101053074b
LarryWaj <larryezeawaji@gmail.com>
2022-09-12 16:27:36 -0700

    The lower case of the alphabets except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,8 +13,8 @@ int main(void)
 {
 	char low, e, q;
 
-	e= 'e';
-	q= 'q';
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 {
@@ -22,6 +22,6 @@ int main(void)
 		putchar(low);
 }
 	putchar('\n');
-	
+
 	return (0);
 }
