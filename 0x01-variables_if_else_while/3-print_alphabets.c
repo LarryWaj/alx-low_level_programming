3979913780f20ffcdcee55de7a06b86ce21a87a2
LarryWaj <larryezeawaji@gmail.com>
2022-09-12 16:13:03 -0700

    Lower, then Uppercase alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,13 +14,13 @@ char low;
 
 for (low = 'a'; low <= 'z'; low++)
 {
-        putchar(low);
+	putchar(low);
 }
 for (low = 'A'; low <= 'Z'; low++)
 {
 	putchar(low);
 }
-        putchar('\n');
+	putchar('\n');
 
 return (0);
 }
